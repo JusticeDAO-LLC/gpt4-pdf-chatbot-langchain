4442d1940a2f18b07790e888bc29390c890b3cff
Benjamin Barber <starworks5@gmail.com>
2023-03-28 22:18:04 -0700

    added header links, and an additional chat page.

--- components/layout.tsx
+++ components/layout.tsx
@@ -7,10 +7,37 @@ export default function Layout({ children }: LayoutProps) {
     <div className="mx-auto flex flex-col space-y-4">
       <header className="container sticky top-0 z-40 bg-white">
         <div className="h-16 border-b border-b-slate-200 py-4">
-          <nav className="ml-4 pl-6">
-            <a href="#" className="hover:text-slate-600 cursor-pointer">
-              Home
-            </a>
+          <nav className="ml-4 pl-6 align-middle text-align-center">
+            <span className="pl-6 space-x-4">
+              <a href="/" className="text-2xl hover:text-slate-600 cursor-pointer">
+                  Home
+              </a>
+            </span>
+            <span className="pl-6 space-x-4">
+              <a href="/laws" className="text-2xl hover:text-slate-600 cursor-pointer">
+                  Laws
+              </a>
+            </span>
+            <span className="pl-6 space-x-4">    
+              <a href="/decisions" className="text-2xl hover:text-slate-600 cursor-pointer">
+                  Decisions
+              </a>
+            </span>
+            <span className="pl-6 space-x-4">    
+              <a href="/dictionary" className="text-2xl hover:text-slate-600 cursor-pointer">
+                  Dictionary
+              </a>
+             </span>
+            <span className="pl-6 space-x-4">    
+              <a href="/lawyers" className="text-2xl hover:text-slate-600 cursor-pointer">
+                  Lawyers
+              </a>
+            </span>
+            <span className="pl-6 space-x-4">    
+              <a href="/chat" className="text-2xl hover:text-slate-600 cursor-pointer">
+                  Chat
+              </a>
+            </span>
           </nav>
         </div>
       </header>
